527900828391e8e2d5177de106b998209d1088b5
PranavAngiya <pranavangiya@gmail.com>
2025-02-09 16:54:11 -0500

    Simplified API adding

---
+++ src/server/routes/api/apiIndex.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Get all routes from auth
+// For example:  "const signupRoute = require('./signup');"
+
+// Use all the routes
+// For example: "router.use('/signup', signupRoute);"
+
+export default router;
--- src/server/routes/routesIndex.js
+++ src/server/routes/routesIndex.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 // Get all routes from api and auth
 const authRoutes = require('./auth/authIndex');
+// UNCOMMENT THIS WHEN APIs HAVE BEEN ADDED
+// UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT
 // const apiRoutes = require('./api');
+// UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT
 
 // Use all routes from api and auth
+
+// UNCOMMENT THIS WHEN APIs HAVE BEEN ADDED
+// UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT
 // router.use('/api', apiRoutes);
+// UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT UNCOMMENT
+
 router.use('/auth', authRoutes);
 
 module.exports = router;
