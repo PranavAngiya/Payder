448e4501d11f30aa236ef9473aed04f1cfff0998
logan pasternak <loganpasternak@cam-25-0-142.cam.ruw.rutgers.edu>
2025-02-19 14:09:25 -0500

    can now see account balance when logged in on top right corner of screen

--- src/server/routes/api/apiIndex.js
+++ src/server/routes/api/apiIndex.js
@@ -4,11 +4,11 @@ const router = express.Router();
 // Get all routes from auth
 // For example:  "const signupRoute = require('./signup');"
 const traderRoute = require('./trader');
-const retrieveBalance = require('./retrieveBalance')
+const balance = require('./balance')
 
 // Use all the routes
 // For example: "router.use('/signup', signupRoute);"
 router.use('/trader', traderRoute);
-router.use('/balance', retrieveBalance)
+router.use('/balance', balance)
 
 module.exports = router;
