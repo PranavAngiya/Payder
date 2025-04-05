a33a9c992ca48c90ba43a53409f0dbee9a7b785d
logan pasternak <loganpasternak@mac.mynetworksettings.com>
2025-04-05 10:08:07 -0400

    during sign up can now set initial sentiment data parameters

--- src/server/routes/auth/authIndex.js
+++ src/server/routes/auth/authIndex.js
@@ -4,9 +4,11 @@ const router = express.Router();
 // Get all routes from auth
 const signupRoute = require('./signup');
 const loginRoute = require('./login');
+const sentimentRoute = require('./set_sentiment');
 
 // Use all the routes
 router.use('/signup', signupRoute);
 router.use('/login', loginRoute);
+router.use('/set_sentiment', sentimentRoute);
 
 module.exports = router;
